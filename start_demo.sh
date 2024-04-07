516f8cb4048c8dc93d01983f84b4b56aa0dfbfc7
Khush Agrawal <agrawalkhush2000@gmail.com>
2024-04-07 12:08:27 -0400

    Refactor - make more system agnostic and use loops

--- start_demo.sh
+++ start_demo.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-# System specific variables
-PERCIV_VENV_PATH='/home/shahram95/perciv_venv/bin/activate'
-PERCIV_WS_PATH='/home/shahram95/perciv_ws'
-PERCIV_ROS_PATH='/opt/ros/foxy/setup.bash'
+echo 'Launching PERCIV demo'
+
+USER_HOME=$(eval echo "~$USER")
+
+# Detect ROS version
+if [ -d "/opt/ros/foxy" ]; then
+    perciv_ros_version="foxy"
+elif [ -d "/opt/ros/humble" ]; then
+    perciv_ros_version="humble"
+else
+    echo "Error: Unknown ROS version or ROS not installed"
+    exit 1  # Exit with a non-zero status to indicate an error
+fi
+
+PERCIV_VENV_SOURCE_PATH="$USER_HOME/perciv_venv/bin/activate"
+PERCIV_WS_PATH="$USER_HOME/perciv_ws"
+PERCIV_ROS_SOURCE_PATH="/opt/ros/$perciv_ros_version/setup.bash"
 PERCIV_ROS_DOMAIN_ID=100
 
-# Common perciv stuff
-make_perciv="conda deactivate && source $PERCIV_VENV_PATH && source $PERCIV_ROS_PATH && source $PERCIV_WS_PATH/install/setup.bash && cd $PERCIV_WS_PATH/src"
+# Sanity checks
+if [ ! -f $PERCIV_VENV_SOURCE_PATH ]; then
+    echo "Error: PERCIV virtual env not found"
+    exit 1
+fi
+if [ ! -d $PERCIV_WS_PATH ]; then
+    echo "Error: PERCIV workspace directory not found"
+    exit 1
+fi
 
-echo 'Launching PERCIV demo'
+# Common perciv stuff
+setup_perciv_shell="conda deactivate && "
+setup_perciv_shell+="source $PERCIV_VENV_SOURCE_PATH && "
+setup_perciv_shell+="source $PERCIV_ROS_SOURCE_PATH && "
+setup_perciv_shell+="source $PERCIV_WS_PATH/install/setup.bash && "
+setup_perciv_shell+="cd $PERCIV_WS_PATH/src"
 
 # Set up environment variables
 export ROS_DOMAIN_ID=$PERCIV_ROS_DOMAIN_ID
@@ -17,30 +42,24 @@ export ROS_DOMAIN_ID=$PERCIV_ROS_DOMAIN_ID
 # Start a new tmux session and created required splits
 tmux new-session -d -s PERCIV_DEMO
 
-tmux split-window -h
-tmux split-window -v
-tmux split-window -v
-tmux select-pane -t 0
-tmux split-window -v
-tmux split-window -v
+commands=(
+    "python3 steering/ros2_controller.py"
+    "python3 safety_checking/collision_detector.py"
+    "ros2 run trajectory_prediction trajectory_prediction"
+    "ros2 run rviz2 rviz2"
+    "cd overlay_generator/src && python detection_visualizer.py"
+    "cd yolo_detection && python test_pose.py"
+)
 
-# Start reading the joystick
-tmux send-keys -t PERCIV_DEMO:0.0 "$make_perciv && python3 steering/ros2_controller.py" C-m
+pane_index=0
+for command in "${commands[@]}"; do
+    tmux send-keys -t PERCIV_DEMO:0.$pane_index "$setup_perciv_shell && $command" C-m
+    tmux split-window -v
+    ((pane_index++))
+done
 
-# Start collision detector
-tmux send-keys -t PERCIV_DEMO:0.1 "$make_perciv && python3 safety_checking/collision_detector.py" C-m
-
-# Start trajectory predicition node
-tmux send-keys -t PERCIV_DEMO:0.2 "$make_perciv && ros2 run trajectory_prediction trajectory_prediction" C-m
-
-# Start rviz
-tmux send-keys -t PERCIV_DEMO:0.3 "$make_perciv && ros2 run rviz2 rviz2" C-m
-
-# Start YOLO detections
-tmux send-keys -t PERCIV_DEMO:0.4 "$make_perciv && cd overlay_generator/src && python detection_visualizer.py" C-m
-
-# Start overlay generation
-tmux send-keys -t PERCIV_DEMO:0.5 "$make_perciv && cd yolo_detection && python test_pose.py" C-m
+# Make things beautiful
+tmux select-layout -t PERCIV_DEMO tiled
 
 # Attach to the tmux session
 tmux attach -t PERCIV_DEMO
