0d71e6ede383f06fd3a9b494dbc5d236fe4c20de
Shahram Najam Syed <shahramnajam.neduet@gmail.com>
2024-04-07 01:11:11 -0400

    Fix some bugs

--- start_demo.sh
+++ start_demo.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
+# System specific variables
 PERCIV_VENV_PATH='/home/shahram95/perciv_venv/bin/activate'
-PERCIV_WS_PATH='/home/shahram95/perciv_ws/src'
+PERCIV_WS_PATH='/home/shahram95/perciv_ws'
 PERCIV_ROS_PATH='/opt/ros/foxy/setup.bash'
 PERCIV_ROS_DOMAIN_ID=100
 
 # Common perciv stuff
-make_perciv="conda deactivate && source $PERCIV_VENV_PATH &&source $PERCIV_ROS_PATH && cd $PERCIV_WS_PATH"
+make_perciv="conda deactivate && source $PERCIV_VENV_PATH && source $PERCIV_ROS_PATH && source $PERCIV_WS_PATH/install/setup.bash && cd $PERCIV_WS_PATH/src"
 
 echo 'Launching PERCIV demo'
-
-# Source ROS and PERCIV workspace
-source $PERCIV_ROS_PATH
-source $PERCIV_WS_PATH/install/setup.bash
-
-# source /opt/ros/humble/setup.bash
-# source /home/perciv/khush/ros2_ws/install/setup.bash
 
 # Set up environment variables
 export ROS_DOMAIN_ID=$PERCIV_ROS_DOMAIN_ID
 
-# Start a new tmux session named 'MySession' detached
+# Start a new tmux session and created required splits
 tmux new-session -d -s PERCIV_DEMO
 
-# Split the first window vertically thrice
 tmux split-window -h
-
-# Split the first pane vertically twice
 tmux split-window -v
 tmux split-window -v
-
-# Split the second pane horizontally
 tmux select-pane -t 0
-
-# Split the second pane vertically twice
 tmux split-window -v
 tmux split-window -v
 
