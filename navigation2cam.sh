05a02a5a6764277a2f1698ec3b9bbb2cbbc09de0
Khush Agrawal <agrawalkhush2000@gmail.com>
2024-11-11 21:38:43 -0500

    Minor improvements

--- navigation2cam.sh
+++ navigation2cam.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo 'Launching PERCIV demo'
 
 
@@ -43,23 +45,32 @@ setup_perciv_shell+="export ROS_DOMAIN_ID=$PERCIV_ROS_DOMAIN_ID && "
 setup_perciv_shell+="export TURTLEBOT3_MODEL=waffle && "
 setup_perciv_shell+="export CYCLONEDDS_URI='<CycloneDDS><Domain><General><NetworkInterfaceAddress>enp3s0</NetworkInterfaceAddress></General></Domain></CycloneDDS>'"
 
-
-
-
 # Start a new tmux session and created required splits
 tmux new-session -d -s $PERCIV_TMUX_NAME
-tmux rename-window -t $PERCIV_TMUX_NAME "scripts"
+tmux rename-window -t $PERCIV_TMUX_NAME "bg_commands"
+tmux new-window -t $PERCIV_TMUX_NAME -n "pnc_commands"
+tmux new-window -t $PERCIV_TMUX_NAME -n "perception_commands"
 
-commands=(
+# commands that don't require monitoring
+bg_commands=(
+    "ros2 launch realsense2_camera rs_multi_camera_launch.py serial_no1:=\'944622073415\' serial_no2:=\'819112073090\' rgb_camera.profile1:=1280,720,30 rgb_camera.profile2:=1280,720,30 depth_module.profile1:=1280,720,30 depth_module.profile2:=1280,720,30 pointcloud.enable1:=false pointcloud.enable2:=false align_depth.enable1:=true align_depth.enable2:=true decimation_filter.enable1:=true decimation_filter.enable2:=true spatial_filter.enable1:=true spatial_filter.enable2:=true temporal_filter.enable1:=true temporal_filter.enable2:=true json_file_path1:=/home/brain/Desktop/MidDensityPreset.json json_file_path2:=/home/brain/Desktop/MidDensityPreset.json enable_infra11:=true enable_infra21:=true enable_infra12:=true enable_infra22:=true camera_name1:=camera1 camera_name2:=camera2"
+    "cd /home/brain/perciv_ws/src/aruco_pose_estimation/scripts && python3 pose_converter.py"
+
+    "rviz2"
+    "ros2 launch car_sim_gazebo world.launch.py"
+
+    "echo $HOME"
+)
+
+pnc_commands=(
     "python3 steering/ros2_controller.py"
     "python3 safety_checking/collision_detector.py"
+    "cd /home/brain/perciv_ws/src/aruco_pose_estimation/scripts && python3 pose_converter.py"
 
-    # "ros2 run trajectory_prediction trajectory_prediction"
-    # "cd overlay_generator/src && python detection_visualizer.py"
-    # "cd yolo_detection && python test_pose.py"
+    "echo $HOME"
+)
 
-    "ros2 launch realsense2_camera rs_multi_camera_launch.py serial_no1:=\'944622073415\' serial_no2:=\'819112073090\' rgb_camera.profile1:=1280,720,30 rgb_camera.profile2:=1280,720,30 depth_module.profile1:=1280,720,30 depth_module.profile2:=1280,720,30 pointcloud.enable1:=false pointcloud.enable2:=false align_depth.enable1:=true align_depth.enable2:=true decimation_filter.enable1:=true decimation_filter.enable2:=true spatial_filter.enable1:=true spatial_filter.enable2:=true temporal_filter.enable1:=true temporal_filter.enable2:=true json_file_path1:=/home/brain/Desktop/MidDensityPreset.json json_file_path2:=/home/brain/Desktop/MidDensityPreset.json enable_infra11:=true enable_infra21:=true enable_infra12:=true enable_infra22:=true camera_name1:=camera1 camera_name2:=camera2"
-
+perception_commands=(
     # "cd ~/Desktop/FVD_perception_pipeline && python3 rv_bev_stitched_depth.py"
     # For integration with Shahram's UX stuff changed above to this:
     "cd ~/Desktop/FVD_perception_pipeline && python3 rv_bev_stitched_depth_nb.py"
@@ -67,21 +78,33 @@ commands=(
     "cd ~/Desktop/FVD_perception_pipeline && python3 3d_deproject.py"
     "cd ~/Desktop/Detector_FVD/detection && python3 test_pose.py"
 
-    "cd /home/brain/perciv_ws/src/aruco_pose_estimation/scripts && python3 pose_converter.py"
-
-    "rviz2"
-    "ros2 launch car_sim_gazebo world.launch.py"
-    # "ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True"
-    # "ros2 launch turtlebot3_navigation2 navigation2.launch.py map:=$HOME/perciv_navigation_ws/src/perciv-sim/src/maps/map.yaml use_sim_time:=True"
     "echo $HOME"
-    # "ros2 service call /reset_odom std_srvs/srv/Empty '{}'"
 )
 
+# "ros2 service call /reset_odom std_srvs/srv/Empty '{}'"
+
+
 pane_index=0
-for command in "${commands[@]}"; do
-    tmux send-keys -t $PERCIV_TMUX_NAME:0.$pane_index "$setup_perciv_shell && $command" C-m
-    tmux split-window -v
-    tmux select-layout -t $PERCIV_TMUX_NAME tiled
+for command in "${bg_commands[@]}"; do
+    tmux send-keys -t $PERCIV_TMUX_NAME:bg_commands.$pane_index "$setup_perciv_shell && $command" C-m
+    tmux split-window -t $PERCIV_TMUX_NAME:bg_commands -v
+    tmux select-layout -t $PERCIV_TMUX_NAME:bg_commands tiled
+    ((pane_index++))
+done
+
+pane_index=0
+for command in "${pnc_commands[@]}"; do
+    tmux send-keys -t $PERCIV_TMUX_NAME:pnc_commands.$pane_index "$setup_perciv_shell && $command" C-m
+    tmux split-window -t $PERCIV_TMUX_NAME:pnc_commands -v
+    tmux select-layout -t $PERCIV_TMUX_NAME:pnc_commands tiled
+    ((pane_index++))
+done
+
+pane_index=0
+for command in "${perception_commands[@]}"; do
+    tmux send-keys -t $PERCIV_TMUX_NAME:perception_commands.$pane_index "$setup_perciv_shell && $command" C-m
+    tmux split-window -t $PERCIV_TMUX_NAME:perception_commands -v
+    tmux select-layout -t $PERCIV_TMUX_NAME:perception_commands tiled
     ((pane_index++))
 done
 
@@ -92,4 +115,4 @@ done
 # bash "$SCRIPT_DIR/start_bev.sh"
 
 # Attach to the tmux session
-tmux attach-session -t $PERCIV_TMUX_NAME:0
+tmux attach-session -t $PERCIV_TMUX_NAME:pnc_commands
